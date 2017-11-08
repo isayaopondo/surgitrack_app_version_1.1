b6ce6e8db61352b962566bd25071389de94aa9c8
Isaya Opondo <opondo.isaiah@gmail.com>
2017-11-08 18:40:10 +0300

    seed with content

---
+++ application/migrations/users_seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`user_id`, `username`, `first_name`, `last_name`, `email`, `phone_number`, `auth_level`, `banned`, `passwd`, `passwd_recovery_code`, `passwd_recovery_date`, `passwd_modified_at`, `accountsadmin_id`, `token`, `last_login`, `created_at`, `modified_at`)
+VALUES
+	(3506747768, 'admin', NULL, NULL, 'admin@admin.com', NULL, 9, '0', '$2y$11$VEEUoH0CC0RBmewmAgBaZOb6UGo5pujL4/667lnX/Mi4lhLdltrbu', NULL, NULL, NULL, NULL, NULL, '2017-11-04 13:13:33', '2017-10-02 19:04:42', '2017-11-04 12:13:33');
