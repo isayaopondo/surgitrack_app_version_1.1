4de547e7056b067df3069280dce207bba2955a5b
Isaya Opondo <opondo.isaiah@gmail.com>
2018-01-29 18:22:46 +0300

    Auth Password Pathway

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -445,6 +445,8 @@ class Auth extends MY_Controller
 
     }
 
+
+
     // --------------------------------------------------------------
 
     /**
@@ -515,6 +517,148 @@ class Auth extends MY_Controller
 
     }
 
+    /**
+     * User recovery form
+     */
+    public function set_password($email='')
+    {
+        // Load resources
+        $this->load->model('Authorization/authorization_model');
+
+        /// If IP or posted email is on hold, display message
+        if ($on_hold = $this->authentication->current_hold_status(TRUE)) {
+            //$view_data['disabled'] = 1;
+        } else {
+            //$this->tokens->match &&
+
+            // If the form post looks good
+            if ($email) {
+                if ($user_data = $this->authorization_model->get_recovery_data($email)) {
+                    // Check if user is banned
+                    if ($user_data->banned == '1') {
+                        // Log an error if banned
+                        $this->authentication->log_error($email);
+
+                        // Show special message for banned user
+                       // $view_data['banned'] = 1;
+                    } else {
+                        /**
+                         * Use the authentication libraries salt generator for a random string
+                         * that will be hashed and stored as the password recovery key.
+                         * Method is called 4 times for a 88 character string, and then
+                         * trimmed to 72 characters
+                         */
+                        $recovery_code = substr($this->authentication->random_salt()
+                            . $this->authentication->random_salt()
+                            . $this->authentication->random_salt()
+                            . $this->authentication->random_salt(), 0, 72);
+
+                        // Update user record with recovery code and time
+                        $this->authorization_model->update_user_raw_data(
+                            $user_data->user_id,
+                            [
+                                'passwd_recovery_code' => $this->authentication->hash_passwd($recovery_code),
+                                'passwd_recovery_date' => date('Y-m-d H:i:s')
+                            ]
+                        );
+
+                        // Set the link protocol
+                        $link_protocol = USE_SSL ? 'https' : NULL;
+
+                        // Set URI of link
+                        $link_uri = 'auth/set_password_verification/' . $user_data->user_id . '/' . $recovery_code;
+                        $special_link = anchor(
+                            site_url($link_uri, $link_protocol),
+                            site_url($link_uri, $link_protocol),
+                            'target ="_blank"'
+                        );
+
+                        //$view_data['special_link'] = $special_link;
+
+                        $this->send_password_creation_mail($special_link, $email,$user_data->username);
+                       // $view_data['confirmation'] = 1;
+
+
+                    }
+                } // There was no match, log an error, and display a message
+                else {
+                    // Log the error
+                    $this->authentication->log_error($this->input->post('email', TRUE));
+
+                  //  $view_data['no_match'] = 1;
+                }
+            }
+
+        }
+
+       // $this->_render_page('auth/recover_form', (isset($view_data)) ? $view_data : '', false);
+
+    }
+
+
+
+    public function set_password_verification($user_id = '', $recovery_code = '')
+    {
+        /// If IP is on hold, display message
+        if ($on_hold = $this->authentication->current_hold_status(TRUE)) {
+            $view_data['disabled'] = 1;
+        } else {
+            // Load resources
+            $this->load->model('Authorization/authorization_model');
+
+            if (
+                /**
+                 * Make sure that $user_id is a number and less
+                 * than or equal to 10 characters long
+                 */
+                is_numeric($user_id) && strlen($user_id) <= 10 &&
+
+                /**
+                 * Make sure that $recovery code is exactly 72 characters long
+                 */
+                strlen($recovery_code) == 72 &&
+
+                /**
+                 * Try to get a hashed password recovery
+                 * code and user salt for the user.
+                 */
+                $recovery_data = $this->authorization_model->get_recovery_verification_data($user_id)) {
+                /**
+                 * Check that the recovery code from the
+                 * email matches the hashed recovery code.
+                 */
+                if ($recovery_data->passwd_recovery_code == $this->authentication->check_passwd($recovery_data->passwd_recovery_code, $recovery_code)) {
+                    $view_data['user_id'] = $user_id;
+                    $view_data['username'] = $recovery_data->username;
+                    $view_data['recovery_code'] = $recovery_data->passwd_recovery_code;
+                } // Link is bad so show message
+                else {
+                    $view_data['recovery_error'] = 1;
+
+                    // Log an error
+                    $this->authentication->log_error('');
+                }
+            } // Link is bad so show message
+            else {
+                $view_data['recovery_error'] = 1;
+
+                // Log an error
+                $this->authentication->log_error('');
+            }
+
+            /**
+             * If form submission is attempting to change password
+             */
+            if ($this->tokens->match) {
+                $this->authorization_model->recovery_password_change();
+            }
+        }
+
+
+        $this->_render_page('auth/set_password', (isset($view_data)) ? $view_data : '', false);
+
+    }
+
     private function send_recovery_mail($special_link, $email, $username, $mailtype = 'recover')
     {
         $this->load->library('notificationmanager');
@@ -525,6 +669,19 @@ class Auth extends MY_Controller
         $this->notificationmanager->sendMail(0, $mailtype, 'Recover Password', $email, $info);
 
     }
+
+    private function send_password_creation_mail($special_link, $email, $username, $mailtype = 'set_password')
+    {
+        $this->load->library('notificationmanager');
+        $info = array(
+            'special_link' => $special_link,
+            'username'=>$username,
+        );
+        $this->notificationmanager->sendMail(0, $mailtype, 'Set Password', $email, $info);
+
+    }
+
+
 
     // --------------------------------------------------------------
 
